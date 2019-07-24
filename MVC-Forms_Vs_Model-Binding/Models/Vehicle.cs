8b92567f84a00f2b6b414ef07ea97c81228d537b
Calvin Sithideth <calvin.sithideth@gmail.com>
2019-07-24 16:06:15 -0700

    Added starting Vehicle model

---
+++ MVC-Forms_Vs_Model-Binding/Models/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Forms_Vs_Model_Binding.Models
+{
+    public class Vehicle
+    {
+        public int VehicleID { get; set; }
+
+        public int NumWheels { get; set; }
+
+        public string DriveTrain { get; set; }
+
+        public string Transmission { get; set; }
+
+        public string Brand { get; set; }
+
+        public string BrandCountry { get; set; }
+
+        public int Year { get; set; }
+
+        public int TotalMileage { get; set; }
+
+        public bool IsNew { get; set; }
+
+        public string ModelName { get; set; }
+
+        public int CityMPG { get; set; }
+
+        public int HwyMPG { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
